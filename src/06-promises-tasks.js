d1a7e346391f07d0402d63ab10e8230c2147ded6
Nduchin <nduchin@mail.ru>
2023-04-20 20:21:27 +0300

    feat: complete various tests

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,11 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((res)=>{
+    if (typeof isPositiveAnswer != 'boolean'){throw new Error('Wrong parameter is passed! Ask her again.')}
+    res(!!isPositiveAnswer)
+  }).then((res)=>(res)?'Hooray!!! She said "Yes"!':'Oh no, she said "No".')
 }
 
 
@@ -48,8 +51,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array)
 }
 
 /**
@@ -71,8 +74,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array)
 }
 
 /**
